4f7693e05b95fa190000c4ce5a7d879fdbb21380
cale312 <zikisacalesogaxa@gmail.com>
2017-08-03 16:03:24 +0200

    did something

--- controllers/manageLogin.js
+++ controllers/manageLogin.js
@@ -1,26 +1,24 @@
-// check if the username already exists in the database
-// if it does not render page to create a team
-// if it does render page with current team stats
-//
+module.exports = function(app) {
 
+  const managers = require('../models/newManagerModel')
 
-module.exports = function(name, fn) {
+  app.get('/login', function(req, res) {
+    res.render('login');
+  });
 
-  const managers = require('../models/newManagerModel');
-
-  managers.findOne({
-    fullName: name
-  }).then(function(result) {
-    if (result) {
-      return 'exist'
-    } else {
-      managers.create({
-        fullName: name
-      }).then(function(result) {
-        if (result) {
-          return 'register'
-        }
-      });
-    }
+  app.post('/login', function(req, res) {
+    const name = req.body.username;
+    managers.findOne({
+      fullName: name
+    }).then(function(result){
+      if (result) {
+        res.render('team', {manager: name});
+      } else {
+        managers.create({
+          fullName: name
+        });
+        res.render('select');
+      }
+    });
   });
 }
--- server.js
+++ server.js
@@ -21,41 +21,15 @@ app.set('view engine', 'handlebars');
 mongoose.Promise = global.Promise;
 
 // controller functions
-const managers = require('./models/newManagerModel');
+const managerLogin = require('./controllers/manageLogin');
 
 // routes
 app.get('/', function(req, res) {
   res.render('index', {});
 });
 
-// app.get('/register', function(req, res){
-//   res.render('register');
-// });
-//
-// app.post('/register', function(req, res){
-//   console.log(req.body);
-//   res.redirect('/register')
-// });
-
-app.get('/login', function(req, res) {
-  res.render('login');
-});
-
-app.post('/login', function(req, res) {
-  const name = req.body.username;
-  managers.findOne({
-    fullName: name
-  }).then(function(result){
-    if (result) {
-      res.render('team', {manager: name});
-    } else {
-      managers.create({
-        fullName: name
-      });
-      res.render('select');
-    }
-  });
-});
+// call controller functions
+managerLogin(app);
 
 const port = process.env.PORT || 8000;
 app.listen(port, function() {
